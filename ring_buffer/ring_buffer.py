31a6f6624e13cf5e8a998be6e95d8df049de8dac
Zurglots <bfeole@gmail.com>
2019-10-18 13:55:09 -0400

    Ring buffer completed

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,27 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+    # adds element to buffer
+    def append(self, item):
+        # set passed in item to current buffer position/index
+        self.storage[self.current] = item
+        # if we progress past our list capacity set the current index back to 0, replacing 'least' used item
+        if self.current < self.capacity - 1:
+            # sets up next index to be updated
+            self.current += 1
+        else:
+            # returns us to start of list if capacity is met
+            self.current = 0
 
-  def get(self):
-    pass
+    # return all elements in the buffer in a list in given order
+    # should not return None values, even if present
+    def get(self):
+        items = []
+        for i in self.storage:
+            if i is not None:
+                items.append(i)
+        print(items)
+        return items
